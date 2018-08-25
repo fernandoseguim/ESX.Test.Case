e095e6604df93f399fcb65a4a926a893424725de
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-25 20:47:28 -0300

    create brand entity

---
+++ src/ESX.Test.Case.Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿using ESX.Test.Case.Shared;
+using System;
+
+namespace ESX.Test.Case.Domain.Entities
+{
+	public sealed class Brand : Entity
+	{
+		public Brand(string name)
+		{
+			this.Name = name;
+			if(string.IsNullOrWhiteSpace(this.Name)) 
+				throw new ArgumentNullException($"The brand name can not be null or whitespace");
+		}
+
+		public string Name { get; }
+	}
+}
---
+++ test/ESX.Test.Case.Tests/Domain/Entities/BrandTests.cs
@@ -0,0 +1,28 @@
+﻿using ESX.Test.Case.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ESX.Test.Case.Tests.Domain.Entities
+{
+	[TestClass]
+	public class BrandTests : UnitTestBase
+	{
+		[TestMethod]
+		[Description("Given that name is null, " +
+		             "when trying to create a new brand, " +
+		             "then should throw argument null exception")]
+		public void Should_throw_argument_null_exception_when_trying_to_create_a_new_brand_with_null_name()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new Brand(null));
+		}
+
+		[TestMethod]
+		[Description("Given that name is withspace, " +
+		             "when trying to create a new brand, " +
+		             "then should throw argument null exception")]
+		public void Should_throw_argument_null_exception_when_trying_to_create_a_new_brand_with_withspace_name()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new Brand(""));
+		}
+	}
+}
