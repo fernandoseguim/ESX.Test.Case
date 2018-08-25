06f9d17cc63d1bf5d7715fb8217d47213e0241bf
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-25 20:46:57 -0300

    create value object password

---
+++ src/ESX.Test.Case.Domain/ValueObjects/Password.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ESX.Test.Case.Domain.ValueObjects
+{
+	public class Password
+	{
+		private const string PASSWORD_PATTERN = @"^\w{8,}$";
+
+		private readonly string value;
+
+		public Password(string value)
+		{
+			this.value = value;
+			if (Regex.IsMatch(this.value ?? "", PASSWORD_PATTERN) == false)
+				throw new ArgumentException($"The password value address is invalid");
+		}
+		
+		public string MaskedValue
+		{
+			get
+			{
+				var bytes = Encoding.ASCII.GetBytes(this.value);
+				return Convert.ToBase64String(bytes);
+			}
+		}
+	}
+}
---
+++ test/ESX.Test.Case.Tests/Domain/ValueObjects/PasswordTests.cs
@@ -0,0 +1,31 @@
+﻿using ESX.Test.Case.Domain.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ESX.Test.Case.Domain.Entities;
+
+namespace ESX.Test.Case.Tests.Domain.ValueObjects
+{
+	[TestClass]
+	public class PasswordTests : UnitTestBase
+	{
+		[TestMethod]
+		[Description("Given that password value is invalid, " +
+		             "when trying to create a new password, " +
+		             "then should throw argument exception")]
+		public void Should_throw_argument_exception_when_trying_to_create_a_new_password_with_invalid_value()
+		{
+			Assert.ThrowsException<ArgumentException>(() => new Password(this.MockString(5)));
+		}
+
+		[TestMethod]
+		[Description("Given that password value is valid, " +
+		             "when creating a new password, " +
+		             "then should mask password value")]
+		public void Should_mask_value_when_creating_a_new_password_with_valid_value()
+		{
+			var value = this.MockString(8);
+			var password = new Password(value);
+			Assert.AreNotEqual(value, password.MaskedValue);
+		}
+	}
+}
