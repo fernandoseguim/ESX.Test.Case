1d55a32d4df38fab12dbc61bedd54e379c8faa1d
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-25 20:47:38 -0300

    create asset entity

---
+++ src/ESX.Test.Case.Domain/Entities/Asset.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ESX.Test.Case.Shared;
+
+namespace ESX.Test.Case.Domain.Entities
+{
+	public sealed class Asset : Entity
+	{
+		public Asset(string name, Brand brand)
+		{
+			this.Name = name;
+			if(string.IsNullOrWhiteSpace(this.Name)) 
+				throw new ArgumentNullException($"The asset name can not be null or whitespace");
+
+			this.Brand = brand ?? throw new ArgumentNullException($"The asset brand can not be null");
+			this.GenerateassetRegisterNumber();
+		}
+
+		public string Name { get; }
+		public Brand Brand { get; }
+		public string Description { get; private set; }
+		public string Registry { get; private set; }
+
+		public void AddDescription(string description) 
+			=> this.Description = description;
+
+		private void GenerateassetRegisterNumber() 
+			=> this.Registry = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 8);
+	}
+}
---
+++ test/ESX.Test.Case.Tests/Domain/Entities/AssetTests.cs
@@ -0,0 +1,66 @@
+﻿using ESX.Test.Case.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ESX.Test.Case.Tests.Domain.Entities
+{
+	[TestClass]
+	public class AssetTests : UnitTestBase
+	{
+		private readonly Brand brand;
+		public AssetTests() => 
+			this.brand = new Brand(base.MockString());
+
+		[TestMethod]
+		[Description("Given that name is null, " +
+		             "when trying to create a new asset, " +
+		             "then should throw argument null exception")]
+		public void Should_throw_argument_null_exception_when_trying_to_create_a_new_asset_with_null_name()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new Asset(null, this.brand));
+		}
+
+		[TestMethod]
+		[Description("Given that name is withspace, " +
+		             "when trying to create a new asset, " +
+		             "then should throw argument null exception")]
+		public void Should_throw_argument_null_exception_when_trying_to_create_a_new_asset_with_withspace_name()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new Asset("", this.brand));
+		}
+
+		[TestMethod]
+		[Description("Given that brand is null, " +
+		             "when trying to create a new asset, " +
+		             "then should throw argument null exception")]
+		public void Should_generate_a_register_number_when_trying_to_create_a_new_asset_with_null_brand()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new Asset(base.MockString(), null));
+		}
+
+		[TestMethod]
+		[Description("Given that name is valid, " +
+		             "when trying to create a new asset, " +
+		             "then should generate a registry number")]
+		public void Should_generate_a_register_number_when_trying_to_create_a_new_asset_with_valid_name()
+		{
+			var asset = new Asset(base.MockString(), this.brand);
+
+			Assert.IsNotNull(asset.Registry);
+		}
+
+		[TestMethod]
+		[Description("Given that I add a asset description, " +
+		             "when I get the description," +
+		             "then should return the same description added")]
+		public void Should_return_the_same_description_added_when_I_get_the_description()
+		{
+			var asset = new Asset(base.MockString(), this.brand);
+			var description = this.MockString(10);
+
+			asset.AddDescription(description);
+
+			Assert.IsNotNull(asset.Description);
+		}
+	}
+}
