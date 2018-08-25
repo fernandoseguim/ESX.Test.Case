c0255849fd43c398e4c5974f893d993be8bf17da
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-25 20:09:06 -0300

    create abstract entity

---
+++ src/ESX.Test.Case.Shared/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Flunt.Notifications;
+
+namespace ESX.Test.Case.Shared
+{
+	public abstract class Entity
+	{
+		protected Entity() => this.Id = Guid.NewGuid();
+
+		public Guid Id { get; }
+	}
+}
