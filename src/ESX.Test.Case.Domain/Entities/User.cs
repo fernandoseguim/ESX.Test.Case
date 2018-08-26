b0ea1703c1ba12cb166c544fd37e28c7f6c6691c
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-26 02:33:07 -0300

    create user entity

---
+++ src/ESX.Test.Case.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ESX.Test.Case.Domain.ValueObjects;
+using ESX.Test.Case.Shared;
+
+namespace ESX.Test.Case.Domain.Entities
+{
+	public class User : Entity
+	{
+		public User(string name, Email email, Password password)
+		{
+			this.Name = name;
+			this.Email = email ?? throw new ArgumentNullException($"The user email can not be null");
+			this.Password = password ?? throw new ArgumentNullException($"The user password can not be null");
+			
+			if (string.IsNullOrWhiteSpace(this.Name))
+				throw new ArgumentNullException($"The user name can not be null or whitespace");
+		}
+
+		public string Name { get; }
+
+		public Email Email { get; }
+
+		public Password Password { get; }
+	}
+}
---
+++ test/ESX.Test.Case.Tests/Domain/Entities/UserTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ESX.Test.Case.Domain.Entities;
+using ESX.Test.Case.Domain.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ESX.Test.Case.Tests.Domain.Entities
+{
+	[TestClass]
+	public class UserTests : UnitTestBase
+	{
+		private readonly Email email;
+		private readonly Password password;
+
+		public UserTests()
+		{
+			this.email = new Email("fernando.seguim@gmail.com");
+			this.password = new Password(this.MockString());
+		}
+
+		[TestMethod]
+		[Description("Given that name is null, " +
+		             "when trying to create a new user, " +
+		             "then should throw argument null exception")]
+		public void Should_throw_argument_null_exception_when_trying_to_create_a_new_user_with_null_name()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new User(null, this.email, this.password));
+		}
+
+		[TestMethod]
+		[Description("Given that name is withspace, " +
+		             "when trying to create a new user, " +
+		             "then should throw argument null exception")]
+		public void Should_throw_argument_null_exception_when_trying_to_create_a_new_user_with_withspace_name()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new User("", this.email, this.password));
+		}
+
+		[TestMethod]
+		[Description("Given that e-mail is null, " +
+		             "when trying to create a new user, " +
+		             "then should throw argument null exception")]
+		public void Should_throw_argument_null_exception_when_trying_to_create_a_new_user_with_null_email()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new User(this.MockString(), null, this.password));
+		}
+
+		[TestMethod]
+		[Description("Given that e-mail is null, " +
+		             "when trying to create a new user, " +
+		             "then should throw argument null exception")]
+		public void Should_throw_argument_null_exception_when_trying_to_create_a_new_user_with_null_password()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new User(this.MockString(), null, this.password));
+		}
+	}
+}
