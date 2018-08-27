4dee46148bafc5a009acd453e73e3777309e2126
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-27 01:24:37 -0300

    create contrat to entity repository

---
+++ src/ESX.Test.Case.Infra/Repositories/IEntityRepository.cs
@@ -0,0 +1,9 @@
+﻿using ESX.Test.Case.Shared.Entities;
+
+namespace ESX.Test.Case.Infra.Repositories
+{
+	public interface IEntityRepository<in TEntity> where TEntity : Entity
+	{
+		void Save(TEntity entity);
+	}
+}
