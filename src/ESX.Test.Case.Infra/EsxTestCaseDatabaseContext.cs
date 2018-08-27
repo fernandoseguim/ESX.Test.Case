5fff6367ae525f7479f1d0c70f6a70e28fd9b111
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-27 01:25:20 -0300

    create database context

---
+++ src/ESX.Test.Case.Infra/EsxTestCaseDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace ESX.Test.Case.Infra
+{
+	public class EsxTestCaseDatabaseContext : IDatabaseContext
+	{
+		public EsxTestCaseDatabaseContext(IDbConnection connection)
+		{
+			this.Connection = connection;
+			this.Connection.Open();
+		}
+
+		public IDbConnection Connection { get; }
+
+		public void Dispose()
+		{
+			if (this.Connection.State != ConnectionState.Closed)
+			{
+				this.Connection.Close();
+			}
+		}
+	}
+}
---
+++ src/ESX.Test.Case.Infra/IDatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data;
+
+namespace ESX.Test.Case.Infra
+{
+	public interface IDatabaseContext : IDisposable
+	{
+		IDbConnection Connection { get; }
+	}
+}
