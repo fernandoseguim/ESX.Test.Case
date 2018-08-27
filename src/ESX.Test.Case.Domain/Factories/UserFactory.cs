ff03285c490be9321e1dcadf52dd651fc8fee89a
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-27 18:38:27 -0300

    create user factory

---
+++ src/ESX.Test.Case.Domain/Factories/UserFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using ESX.Test.Case.Domain.Commands.Request;
+using ESX.Test.Case.Domain.Entities;
+using ESX.Test.Case.Domain.ValueObjects;
+using Flunt.Notifications;
+
+namespace ESX.Test.Case.Domain.Factories
+{
+	public class UserFactory
+	{
+		public static User Create(UserCommand command)
+		{
+			var name = new Name(command.FirstName, command.LastName);
+			var email = new Email(command.Email);
+			var password = new SecurityPassword(command.Password);
+			var user = new User(name, email, password);
+
+			return user;
+		}
+	}
+}
