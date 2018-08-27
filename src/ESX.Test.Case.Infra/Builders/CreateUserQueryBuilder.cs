c8e3ce0ecb2b569d9f3b9b99b9d26fb8c53219f1
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-27 01:27:07 -0300

    create query builder

---
+++ src/ESX.Test.Case.Infra/Builders/CreateUserQueryBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace ESX.Test.Case.Infra.Builders
+{
+	public partial class UserQueryBuilder
+	{
+		public UserQueryBuilder CreateUser()
+		{
+			this.query = @"INSERT INTO Users (UserID, Name, Email, PasswordHash, Salt) 
+							VALUES(@UserID, @Name, @Email, @PasswordHash, @Salt)";
+			this.parameters.Add("UserID", this.user.Id);
+			this.parameters.Add("Name", this.user.Name.ToString());
+			this.parameters.Add("Email", this.user.Email.ToString());
+			this.parameters.Add("PasswordHash", this.user.Password.HashValue);
+			this.parameters.Add("Salt", this.user.Password.Salt);
+			return this;
+		}
+	}
+}
---
+++ src/ESX.Test.Case.Infra/Builders/UserQueryBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Dapper;
+using ESX.Test.Case.Domain.Entities;
+
+namespace ESX.Test.Case.Infra.Builders
+{
+	public partial class UserQueryBuilder
+	{
+		private readonly User user;
+		private readonly DynamicParameters parameters;
+		private string query = "";
+
+		public UserQueryBuilder(User user)
+		{
+			this.user = user ?? throw new ArgumentNullException(nameof(user));
+			this.parameters = new DynamicParameters();
+		}
+
+		public (string, DynamicParameters) Build() =>
+			(this.query, this.parameters);
+
+		private void FormatQuery(string queryItem)
+		{
+			if (this.query != "") { this.query = $"{this.query} AND"; }
+			this.query = $"{this.query} {queryItem}";
+		}
+	}
+}
