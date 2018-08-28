3468a0424d3bb8dda19b77cf06b0c20ad7cb4bd9
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-27 23:20:00 -0300

    implement user controller

---
+++ src/ESX.Test.Case.Api/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using ESX.Test.Case.Domain.Commands.Request;
+using ESX.Test.Case.Shared.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESX.Test.Case.Api.Controllers
+{
+	[ApiVersion("1.0")]
+	[Route("api/v{version:apiVersion}/[controller]")]
+	public class UsersController : Controller
+	{
+		private readonly ICommandHandler<UserCommand> useCommandHandler;
+
+		public UsersController(ICommandHandler<UserCommand> useCommandHandler) 
+			=> this.useCommandHandler = useCommandHandler;
+
+		// POST api/users
+		[HttpPost]  
+		public IActionResult Post([FromBody] UserCommand userCommand)
+		{
+			var result = this.useCommandHandler.Handle(userCommand);
+			
+			return this.StatusCode((int)result.StatusCode, result);
+		}
+	}
+}
